5942aff997750bb1f6c2ee495965eba43dd8c8b7
SONGYI-chen123 <song1577660501@163.com>
2020-09-16 07:12:33 +0800

    mockito test stub

--- src/test/java/mockito/mockitoTest.java
+++ src/test/java/mockito/mockitoTest.java
@@ -4,17 +4,31 @@ import mock.DoorPanel;
 import mock.SecurityCenter;
 import org.junit.jupiter.api.Test;
 import org.mockito.junit.MockitoJUnitRunner;
+import stub.GradeSystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 
 public class mockitoTest {
     SecurityCenter securityCenter;
     @Test
-    public  void test_mock(){
+    public  void mockito_test_mock(){
         DoorPanel mockDoorPanel = mock(DoorPanel.class);
         securityCenter = new SecurityCenter(mockDoorPanel);
         securityCenter.switchOn();
         verify(mockDoorPanel).close();
     }
-    
+
+    @Test
+    public void mockito_test_stub(){
+        GradeSystem stubgradesystem = mock(GradeSystem.class);
+        List<Double> grade = Arrays.asList(90.0, 95.0, 100.0);
+        when(stubgradesystem.gradesFor(99900l)).thenReturn(grade);
+        System.out.println(stubgradesystem.gradesFor(99900l));
+    }
+
 }
